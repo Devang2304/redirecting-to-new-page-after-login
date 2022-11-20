df2f7fe3d7b0a3bd0b9a5aedcf7bbab2f5bb7100
Devang2304 <devangvartak2304@gmail.com>
2022-11-21 00:46:37 +0530

    didn't resolve

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const User = require('./model/adminLoginSchema')
 const { reset } = require('nodemon')
 const { resolve } = require('path')
 const http = require('http');
-
+var isLoggedIn = false;
 mongoose.connect('mongodb+srv://DEVANG23:sGS9G3qDgNVQztE@cluster0.ngb8lfk.mongodb.net/test_project')
 app.use(bodyParser.json())
 app.use(express.static(__dirname))
@@ -20,11 +20,11 @@ app.get('/src/admin', (req, res) => {
     console.log(req.url);
     res.sendFile(path.resolve(__dirname, 'src/admin.html'))
 })
-// app.get('/src/dashboard', (req, res) => {
-//     console.log("dashboard appeared in url");
-//     console.log(req.url);
-//     res.sendFile(path.resolve(__dirname, 'src/dashboard.html'))
-// })
+app.get('/src/dashboard',auth ,(req, res) => {
+    console.log("dashboard appeared in url");
+    console.log(req.url);
+    res.sendFile(path.resolve(__dirname, 'src/dashboard.html'))
+})
 
 app.listen(3000, () => {
     console.log("listening on port 3000...");
@@ -38,19 +38,32 @@ app.post('/login', async (req, res) => {
     }
     if (password == user.password) {
         console.log("you are logged in ...");
+        isLoggedIn=true;
+        if(isLoggedIn==='true'){
+            auth();
+            // return res.sendFile(path.resolve(__dirname, 'src/dashboard.html'))
+            res.redirect('/src/dashboard.html')
+        
+        }
         // res.send({"Success":"Success!"});
         // res.writeHead(200,{'content-type': 'text/html'});
         // res.write("./src/dashboard.html");
         // res.end();
-        res.sendFile(path.join(__dirname, '/src/dashboard.html'));
+        // res.sendFile(path.join(__dirname, '/src/dashboard.html'));
         // res.setHeader('Content-Type', '/src/dashboard.html');
         // const headers=res.getHeader('Content-Type');
         // console.log(headers);
-    // return res.redirect('/src/dashboard.html')
-
-        // return res.sendFile(path.resolve(__dirname, 'src/dashboard.html'))
+        // res.redirect('/src/dashboard.html')
+        
         // return res.json({status:'ok',data:'loggedin'})
+        
     }
     res.json({ status: 'error',error:"invalid username/password" })
 })
 
+function auth() {
+    if(isLoggedIn==='true'){
+        return
+    }
+    res.send('User not registered');
+}
